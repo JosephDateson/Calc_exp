6136af2db4c07fb79a5681e42eeac8fdaebb81b5
yossi <yossi.tamarov@gmail.com>
2017-12-26 05:57:12 +0200

    Try collectstatic

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+import math
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
 	url(r'^calc/', include('polls.urls')),
