5fadf1d9874d4ae6da7588966954a88b1f32f15a
yossi <yossi.tamarov@gmail.com>
2018-01-14 01:36:25 +0200

    All updates but comments and help are done

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 import math
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-	url(r'^calc/', include('polls.urls')),
+    url(r'^', include('polls.urls')),
+	url(r'^result/', include('polls.urls')),
 ]
